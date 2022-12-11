1b0660afa94ee25efeabf166e1e3d3bc91d8eb93
mutievaai <mutievaai@gmail.com>
2022-12-11 23:34:06 +0600

    Dice Roll
    Dice Roll

--- DiceRoll/app/src/main/java/com/example/diceroll/MainActivity.kt
+++ DiceRoll/app/src/main/java/com/example/diceroll/MainActivity.kt
@@ -1,10 +1,9 @@
-package com.example.assignment1
+package com.example.diceroll
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,16 +19,14 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         val cubeRoll = (1..6).random()
         val diceImage: ImageView = findViewById(R.id.dice_image)
-        diceImage.setImageResource(R.drawable.dice_4)
-
-       when(cubeRoll){
-           1 -> diceImage.setImageResource(R.drawable.dice_1)
-           2 -> diceImage.setImageResource(R.drawable.dice_2)
-           3 -> diceImage.setImageResource(R.drawable.dice_3)
-           4 -> diceImage.setImageResource(R.drawable.dice_4)
-           5 -> diceImage.setImageResource(R.drawable.dice_5)
-           6 -> diceImage.setImageResource(R.drawable.dice_6)
-       }
+        when(cubeRoll){
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            6 -> diceImage.setImageResource(R.drawable.dice_6)
+        }
     }
 
 
